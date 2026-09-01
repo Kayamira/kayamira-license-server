6bed7d3e131cc21aea3ca1dff02d07404345b708
Kayamira <sandramashanda86@gmail.com>
2026-09-01 17:38:58 +0200

    Fix comment formatting in worker.js

--- worker.js
+++ worker.js
@@ -216,4 +216,4 @@ function esc(s){return String(s).replaceAll("&","&amp;").replaceAll("<","&lt;").
 if(T)login();
 </script></body></html>`;
 }
-//Kayamira Licence Server deployment
+// Kayamira License Server deployment
